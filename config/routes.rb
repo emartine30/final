d8f15e7b5b2acd0790a595c9f752a13ef47c61f3
Seb <emartine@bates.edu>
2014-05-19 16:26:56 -0400

    added application page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Final::Application.routes.draw do
    get "/about" => "page#about"
    get "/user" => "page#user"
    get "/gigs" => "page#gigs"
+   get "/apps" => "page#apps"
   root "page#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
